462cd49f07d06a3000567363e72dcc86d2b8bbb2
ppred <ppred@DESKTOP-OCKEAKH>
2020-01-05 22:39:03 +0530

    Initial commit

---
+++ src/main/java/com/pp/SimpleServlet.java
@@ -0,0 +1,27 @@
+package com.pp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/hello")
+public class SimpleServlet extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+         throws ServletException, IOException {
+	   
+	  int i=10000;
+	  int j=10000;
+	  int k=10000;
+	  int l=10000;
+	  
+      resp.setContentType("text/plain");
+      resp.getWriter().write("Hello World! Maven Web Project Example.");
+   }
+}
